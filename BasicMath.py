6ad0894094a345cabd8a74573f3f64b8d1c38af7
codevan <15045028+codevan@users.noreply.github.com>
2022-01-25 19:08:39 +0000

    very basic working model

---
+++ BasicMath.py
@@ -0,0 +1,9 @@
+#A simple maths game
+#2nd Python Project
+
+print("Welcome to the maths game!")
+print("What is 1+1?")
+if input() == "2":
+    print("Correct, you brainiac, you!")
+else:
+    print("Wrong, go back to school!")
