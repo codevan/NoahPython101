a2a8dbc70b1a415e147fe52e8ff218d801c0df1e
codevan <15045028+codevan@users.noreply.github.com>
2022-01-25 20:01:09 +0000

    Still working, a work in progress

--- BasicMath.py
+++ BasicMath.py
@@ -1,9 +1,29 @@
 #A simple maths game
 #2nd Python Project
 
+import random
+import string
+
+player_score = 0
+
+
+def getRandomNumber(min, max):
+    return random.randint(min, max)
+
+Number1 = getRandomNumber(1,5)
+print(Number1)
+Number2 = getRandomNumber(1,5)
+print(Number2)
+
 print("Welcome to the maths game!")
-print("What is 1+1?")
-if input() == "2":
+print("What is Number1 + Number2?")
+Answer = Number1 + Number2
+
+
+if input() == "4":
+#if input() == Answer:
     print("Correct, you brainiac, you!")
+    player_score += 1
 else:
     print("Wrong, go back to school!")
+
